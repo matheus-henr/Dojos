9b9e27fd38fe3daa844876897aa045413a93c42b
Matheus Henrique <matheushenrique.dev@gmail.com>
2019-07-16 09:40:49 -0300

    1044

---
+++ Multiplos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Multiplos {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int a, b;
+		
+		a = input.nextInt();
+		b = input.nextInt();
+		
+		
+		if(a % b == 0 || b % a == 0) {
+			System.out.println("Sao Multiplos");
+		}else {
+			System.out.println("Nao sao Multiplos");
+		}
+		
+		
+		input.close();
+	}
+}
